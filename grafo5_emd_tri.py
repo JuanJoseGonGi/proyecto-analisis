c0907f0ba653688cac88212ebe462d6854b63b8f
Juan José González Giraldo <juanjose.eng@gmail.com>
2021-07-11 22:27:45 -0500

    Change prints to just print needed data

--- grafo5_emd_tri.py
+++ grafo5_emd_tri.py
@@ -1,30 +1,30 @@
 import pyphi
 import numpy as np
 
-pyphi.config.load_file('pyphi_config_l1_bi.yml')
+pyphi.config.load_file('pyphi_config_emd_tri.yml')
 
 tpm = np.array([
     [0, 0, 0, 0],
     [0, 1, 1, 1],
     [1, 0, 1, 0],
-    [1, 1, 1, 1],
     [1, 1, 0, 1],
+    [0, 1, 0, 0],
+    [0, 1, 1, 1],
+    [1, 1, 1, 0],
+    [1, 1, 0, 1],
+    [0, 0, 0, 0],
+    [0, 1, 1, 1],
     [1, 0, 1, 0],
-    [1, 1, 1, 1],
-    [1, 0, 1, 0],
-    [1, 0, 1, 0],
-    [1, 1, 1, 1],
-    [1, 0, 1, 0],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 0, 1, 0],
-    [1, 1, 1, 1],
-    [1, 0, 1, 0],
+    [1, 1, 0, 1],
+    [0, 1, 0, 0],
+    [0, 1, 1, 1],
+    [1, 1, 1, 0],
+    [1, 1, 0, 1],
 ])
 
 labels = ('A', 'B', 'C', 'D')
 network = pyphi.Network(tpm, node_labels=labels)
-state = (1, 0, 1, 0)
+state = (0, 1, 0, 0)
 node_indices = (0, 1, 2, 3)
 subsystem = pyphi.Subsystem(network, state, node_indices)
 sia = pyphi.compute.sia(subsystem)
--- grafo5_l1_bi_mc.py
+++ grafo5_l1_bi_mc.py
@@ -7,24 +7,30 @@ tpm = np.array([
     [0, 0, 0, 0],
     [0, 1, 1, 1],
     [1, 0, 1, 0],
-    [1, 1, 1, 1],
     [1, 1, 0, 1],
+    [0, 1, 0, 0],
+    [0, 1, 1, 1],
+    [1, 1, 1, 0],
+    [1, 1, 0, 1],
+    [0, 0, 0, 0],
+    [0, 1, 1, 1],
     [1, 0, 1, 0],
-    [1, 1, 1, 1],
-    [1, 0, 1, 0],
-    [1, 0, 1, 0],
-    [1, 1, 1, 1],
-    [1, 0, 1, 0],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 0, 1, 0],
-    [1, 1, 1, 1],
-    [1, 0, 1, 0],
+    [1, 1, 0, 1],
+    [0, 1, 0, 0],
+    [0, 1, 1, 1],
+    [1, 1, 1, 0],
+    [1, 1, 0, 1],
 ])
 
+cm = np.array([
+    [0, 1, 0, 0],
+    [1, 0, 1, 0],
+    [1, 1, 0, 0],
+    [1, 0, 0, 0],
+])
 labels = ('A', 'B', 'C', 'D')
-network = pyphi.Network(tpm, node_labels=labels)
-state = (1, 0, 1, 0)
+network = pyphi.Network(tpm, cm=cm, node_labels=labels)
+state = (0, 1, 0, 0)
 node_indices = (0, 1, 2, 3)
 subsystem = pyphi.Subsystem(network, state, node_indices)
 sia = pyphi.compute.sia(subsystem)
