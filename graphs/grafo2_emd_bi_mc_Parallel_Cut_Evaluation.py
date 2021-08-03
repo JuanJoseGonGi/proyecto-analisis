6b89f7ae6f4d23c74a88ca1887cf4b6dc9c10ea3
Lucas Bohórquez <lucas.1701716343@ucaldas.edu.co>
2021-08-02 20:49:36 -0500

    Parallel Cut Evaluation

---
+++ graphs/grafo2_emd_bi_mc_Parallel_Cut_Evaluation.py
@@ -0,0 +1,41 @@
+import os
+import pyphi
+import numpy as np
+
+pyphi.config.load_file(
+    os.path.dirname(__file__) + '/../config/pyphi_config_emd_bi_Parallel_Cut_Evaluation.yml')
+
+tpm = np.array([
+    [0, 0, 0, 0],
+    [0, 1, 1, 1],
+    [1, 0, 1, 0],
+    [1, 1, 1, 1],
+    [1, 1, 0, 1],
+    [1, 0, 1, 0],
+    [1, 1, 1, 1],
+    [1, 0, 1, 0],
+    [1, 0, 1, 0],
+    [1, 1, 1, 1],
+    [1, 0, 1, 0],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 0, 1, 0],
+    [1, 1, 1, 1],
+    [1, 0, 1, 0],
+])
+
+cm = np.array([
+    [0, 1, 1, 1],
+    [1, 0, 1, 0],
+    [1, 1, 0, 1],
+    [1, 0, 1, 0],
+])
+labels = ('A', 'B', 'C', 'D')
+network = pyphi.Network(tpm, cm=cm, node_labels=labels)
+state = (1, 0, 1, 0)
+node_indices = (0, 1, 2, 3)
+subsystem = pyphi.Subsystem(network, state, node_indices)
+sia = pyphi.compute.sia(subsystem)
+print("MIP: \n", sia.cut)
+print("Phi: \n Φ = ", sia.phi)
+print("Tiempo: \n", sia.time, "s")
---
+++ graphs/grafo8_emd_bi_mc_Parallel_Cut_Evaluation.py
@@ -0,0 +1,36 @@
+import os
+import pyphi
+import numpy as np
+
+pyphi.config.load_file(
+    os.path.dirname(__file__) + '/../config/pyphi_config_emd_bi_Parallel_Cut_Evaluation.yml')
+
+tpm = np.array([
+[0,0,0,0,0],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],
+[1,1,1,1,1],[1,1,1,1,1]])
+
+cm = np.array([
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1]
+])
+labels = ('A', 'B', 'C', 'D', 'E')
+network = pyphi.Network(tpm, cm=cm, node_labels=labels)
+state = (1, 1, 1, 1, 1)
+node_indices = (0, 1, 2, 3, 4)
+subsystem = pyphi.Subsystem(network, state, node_indices)
+sia = pyphi.compute.sia(subsystem)
+print("MIP: \n", sia.cut)
+print("Phi: \n Φ = ", sia.phi)
+print("Tiempo: \n", sia.time, "s")
